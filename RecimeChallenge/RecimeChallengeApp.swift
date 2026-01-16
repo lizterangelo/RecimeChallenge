7aa23d96c61606381d8a219f0fdd9711f61d2f2d
Lizter Angelo Abrau <lmabrau@up.edu.ph>
2026-01-16 14:22:09 +0800

    Initial Commit

---
+++ RecimeChallenge/RecimeChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecimeChallengeApp.swift
+//  RecimeChallenge
+//
+//  Created by LIZ on 1/16/26.
+//
+
+import SwiftUI
+
+@main
+struct RecimeChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
